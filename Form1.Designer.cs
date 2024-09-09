e79fd4ad8631092231433111c1ba209524cb400e
Aluno <Aluno@59XS314>
2024-09-09 20:51:50 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Visualizador_de_Imagens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            pictureBox1 = new PictureBox();
+            checkBox1 = new CheckBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            showButton = new Button();
+            clearButton = new Button();
+            backgroundButton = new Button();
+            closeButton = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            colorDialog1 = new ColorDialog();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 85F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(checkBox1, 0, 1);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 1, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.Size = new Size(548, 352);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            tableLayoutPanel1.SetColumnSpan(pictureBox1, 2);
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(542, 310);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(3, 319);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(63, 19);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Stretch";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(showButton);
+            flowLayoutPanel1.Controls.Add(clearButton);
+            flowLayoutPanel1.Controls.Add(backgroundButton);
+            flowLayoutPanel1.Controls.Add(closeButton);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.FlowDirection = FlowDirection.RightToLeft;
+            flowLayoutPanel1.Location = new Point(85, 319);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(460, 30);
+            flowLayoutPanel1.TabIndex = 2;
+            // 
+            // showButton
+            // 
+            showButton.Location = new Point(359, 3);
+            showButton.Name = "showButton";
+            showButton.Size = new Size(98, 23);
+            showButton.TabIndex = 3;
+            showButton.Text = "Show a picture";
+            showButton.UseVisualStyleBackColor = true;
+            showButton.Click += showButton_Click;
+            // 
+            // clearButton
+            // 
+            clearButton.Location = new Point(244, 3);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(109, 23);
+            clearButton.TabIndex = 2;
+            clearButton.Text = "Clear the picture";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
+            // backgroundButton
+            // 
+            backgroundButton.Location = new Point(115, 3);
+            backgroundButton.Name = "backgroundButton";
+            backgroundButton.Size = new Size(123, 23);
+            backgroundButton.TabIndex = 1;
+            backgroundButton.Text = "Set the background color";
+            backgroundButton.UseVisualStyleBackColor = true;
+            backgroundButton.Click += backgroundButton_Click;
+            // 
+            // closeButton
+            // 
+            closeButton.Location = new Point(34, 3);
+            closeButton.Name = "closeButton";
+            closeButton.Size = new Size(75, 23);
+            closeButton.TabIndex = 0;
+            closeButton.Text = "Close";
+            closeButton.UseVisualStyleBackColor = true;
+            closeButton.Click += closeButton_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(548, 352);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Picture Viewer";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox pictureBox1;
+        private CheckBox checkBox1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button closeButton;
+        private Button backgroundButton;
+        private Button clearButton;
+        private Button showButton;
+        private OpenFileDialog openFileDialog1;
+        private ColorDialog colorDialog1;
+    }
+}
